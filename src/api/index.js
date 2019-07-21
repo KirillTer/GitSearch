88d20c220f33e933dd58a2348861c7c81fcf4366
Kirill Tereshenko <Vaulttec.Ua@gmail.com>
2019-07-21 23:43:50 +0300

    updated url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import request from 'superagent'
 
 export const searchRepo = async (search) => {
     const {body} = await request.get(
-    `https://api.github.com/search/repositories?q=${search}+language:assembly&sort=stars&order=desc`
+    `https://api.github.com/search/repositories?q=${search}`
     )
     return body
 }
